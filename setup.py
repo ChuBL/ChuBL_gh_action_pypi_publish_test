1979270e58feb9846cb5d20287022d7088498d33
ChuBL <wondered@msn.com>
2024-04-29 15:32:25 -0700

    test 008

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name="chubl_pypi_action_test",
-    version="0.0.7",
+    version="0.0.8",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=[],  # External packages as dependencies
     author="Your Name",
     author_email="your.email@example.com",
     description="A simple example package",
     keywords="example",
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown'
 )
