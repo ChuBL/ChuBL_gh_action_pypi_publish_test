33a8a94218b8bf665d7d6d1c85e80b5945a7d534
ChuBL <wondered@msn.com>
2024-04-28 23:04:59 -0700

    v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chubl_pypi_action_test",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=[],  # External packages as dependencies
     author="Your Name",
