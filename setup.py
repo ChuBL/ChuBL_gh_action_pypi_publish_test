ec39bac7cbef1087564d770a2d8d100750deb87d
ChuBL <wondered@msn.com>
2024-04-29 15:45:10 -0700

    upgrade to v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name="chubl_pypi_action_test",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=[],  # External packages as dependencies
     author="Your Name",
